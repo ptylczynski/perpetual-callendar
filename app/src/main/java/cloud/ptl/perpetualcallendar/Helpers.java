2c35dfc31ee8a6373d1baaf6504aa8e76ccdb614
Piotr Tylczynski <piotr@ptl.cloud>
2021-04-07 21:03:14 +0200

    working days counter add

--- app/src/main/java/cloud/ptl/perpetualcallendar/Helpers.java
+++ app/src/main/java/cloud/ptl/perpetualcallendar/Helpers.java
@@ -1,4 +1,197 @@
 package cloud.ptl.perpetualcallendar;
 
+import android.os.Build;
+import android.widget.DatePicker;
+
+import androidx.annotation.RequiresApi;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Helpers {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void setCurrentDate(DatePicker control){
+        LocalDateTime now = LocalDateTime.now();
+        control.updateDate(
+                now.getYear(),
+                now.getMonth().getValue() - 1,
+                now.getDayOfMonth()
+        );
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDate easterDate(DatePicker datePicker){
+        final int year = datePicker.getYear();
+        final int a = year % 19;
+        final int b = Integer.parseInt(((Double) Math.ceil(year / 100.0)).toString());
+        final int c = year % 100;
+        final int d = Integer.parseInt(((Double) Math.ceil(b / 4.0)).toString());
+        final int e = b % 4;
+        final int f = Integer.parseInt(((Double) ((b + 8) / 25.0)).toString());
+        final int g = Integer.parseInt(((Double) ((b - f + 1) / 3.0)).toString());
+        final int h = (19 * a + b - d + 15) % 30;
+        final int i = Integer.parseInt(((Double) (c / 4.0)).toString());
+        final int k = c % 4;
+        final int l = (32 + 2 * e + 2 * i - h - k) % 7;
+        final int m = Integer.parseInt(((Double) (a + 11.0 * h + 22.0 * l)).toString());
+        final int p = (h + l - 7 * m + 114) % 31;
+
+        final int day = p + 1;
+        final int month = Integer.parseInt(((Double) ((h + l - 7.0 * m + 114.0) / 31.0)).toString());
+
+        return LocalDate.of(
+                year,
+                month,
+                day
+        );
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDate popielecDate(DatePicker datePicker){
+        LocalDate easterDate = Helpers.easterDate(datePicker);
+        return easterDate.minus(46, ChronoUnit.DAYS);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDate corpusDeiDate(DatePicker datePicker){
+        LocalDate easterDate = Helpers.easterDate(datePicker);
+        return easterDate.plus(60, ChronoUnit.DAYS);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static LocalDate adventStartDate(DatePicker datePicker){
+        LocalDate xmas = LocalDate.of(
+                datePicker.getYear(),
+                12,
+                25
+        );
+
+        return xmas.minus(
+                4 * 7 + xmas.getDayOfWeek().getValue(),
+                ChronoUnit.DAYS
+        );
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    public static List<LocalDate> shoppingSundays(DatePicker datePicker){
+        int year = datePicker.getYear();
+        LocalDate januaryLastDay = LocalDate.of(year, 1, 31);
+        LocalDate aprilLastDay = LocalDate.of(year, 4, 30);
+        LocalDate juneLastDay = LocalDate.of(year, 6, 31);
+        LocalDate augustLastDay = LocalDate.of(year, 8, 31);
+        LocalDate xmas = LocalDate.of(year, 12, 25);
+        LocalDate easter = Helpers.easterDate(datePicker);
+        return List.of(
+                januaryLastDay
+                        .minus(
+                                (januaryLastDay.getDayOfWeek().getValue() + 1) % 7,
+                                ChronoUnit.DAYS
+                        ),
+                aprilLastDay
+                        .minus(
+                                (aprilLastDay.getDayOfWeek().getValue() + 1) % 7,
+                                ChronoUnit.DAYS
+                        ),
+                juneLastDay.minus(
+                        (juneLastDay.getDayOfWeek().getValue() + 1) % 7,
+                        ChronoUnit.DAYS
+                ),
+                augustLastDay.minus(
+                        (augustLastDay.getDayOfWeek().getValue() + 1) % 7,
+                        ChronoUnit.DAYS
+                ),
+                xmas.minus(
+                        (xmas.getDayOfWeek().getValue()  + 1) % 7,
+                        ChronoUnit.DAYS
+                ),
+                xmas.minus(
+                        (xmas.getDayOfWeek().getValue() + 1) % 7 + 7,
+                        ChronoUnit.DAYS
+                ),
+                easter.minus(
+                        (easter.getDayOfWeek().getValue() + 1) % 7,
+                        ChronoUnit.DAYS
+                )
+        );
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    public static Integer workingDays(DatePicker start, DatePicker end){
+        List<LocalDate> stateHolidays = new ArrayList<>();
+        Integer amount = 0;
+
+        LocalDate startDate = LocalDate.of(
+                start.getYear(),
+                start.getMonth(),
+                start.getDayOfMonth()
+        );
+
+        LocalDate endDate = LocalDate.of(
+                end.getYear(),
+                end.getMonth(),
+                end.getDayOfMonth()
+        );
+
+        if (endDate.isBefore(startDate)) return 0;
+
+        for(int year = start.getYear(); year < end.getYear(); year++){
+            stateHolidays.addAll(
+                    List.of(
+                            LocalDate.of(
+                                    1,1,year
+                            ),
+                            LocalDate.of(
+                                    6,1,year
+                            ),
+                            LocalDate.of(
+                                    1, 5, year
+                            ),
+                            LocalDate.of(
+                                    3, 5, year
+                            ),
+                            LocalDate.of(
+                                    15, 8, year
+                            ),
+                            LocalDate.of(
+                                    1,11,year
+                            ),
+                            LocalDate.of(
+                                    25, 12, year
+                            ),
+                            LocalDate.of(
+                                    26, 12, year
+                            )
+                    )
+            );
+        }
+        for (
+                LocalDate date :
+                Stream.iterate(
+                        startDate, date -> date.plus(1, ChronoUnit.DAYS)
+                ).limit(
+                        ChronoUnit.DAYS.between(
+                                startDate,
+                                endDate
+                        )
+                ).collect(Collectors.toList())
+        ){
+            if (!stateHolidays.contains(date))
+                if(date.getDayOfWeek().getValue() != 6)
+                    if(date.getDayOfWeek().getValue() != 5)
+                        amount++;
+        }
+        return amount;
+    }
 }
--- app/src/main/java/cloud/ptl/perpetualcallendar/MainActivity.java
+++ app/src/main/java/cloud/ptl/perpetualcallendar/MainActivity.java
@@ -1,14 +1,88 @@
 package cloud.ptl.perpetualcallendar;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DatePicker datePicker_start;
+    private DatePicker datePicker_end;
+
+    private TextView textView_workingDays;
+
+    private Switch switch_movingHolidays;
+    private Switch switch_shoppingSundays;
+
+    private Button button_check;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.datePicker_start = findViewById(R.id.date_picker);
+        this.datePicker_end = findViewById(R.id.date_picker2);
+
+        this.textView_workingDays = findViewById(R.id.textView_working_days_output);
+
+        this.switch_movingHolidays = findViewById(R.id.switch_moving_holidays);
+        this.switch_shoppingSundays = findViewById(R.id.switch_shopping_sat);
+
+        this.button_check = findViewById(R.id.button_calculate);
+
+        Helpers.setCurrentDate(this.datePicker_start);
+        Helpers.setCurrentDate(this.datePicker_end);
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.datePicker_end.setOnDateChangedListener((view, year, monthOfYear, dayOfMonth) -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                this.textView_workingDays.setText(
+                        Helpers.workingDays(
+                                this.datePicker_start,
+                                this.datePicker_end
+                        ).toString()
+                );
+            }
+        });
+        this.datePicker_start.setOnDateChangedListener((view, year, monthOfYear, dayOfMonth) -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                this.textView_workingDays.setText(
+                        Helpers.workingDays(
+                                this.datePicker_start,
+                                this.datePicker_end
+                        ).toString()
+                );
+            }
+        });
+
+        this.switch_shoppingSundays.setOnClickListener(v -> {
+            this.switch_movingHolidays.setChecked(false);
+        });
+
+        this.switch_movingHolidays.setOnClickListener(v -> {
+            this.switch_shoppingSundays.setChecked(false);
+        });
+
+        this.button_check.setOnClickListener(v -> {
+
+        });
     }
 }
